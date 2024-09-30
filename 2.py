cc5052f325ad65ffabc5fbd29a2f57795a234f27
Kedar Dhage <kedardhage0@gmail.com>
2024-09-30 19:02:42 +0530

    Variable and Datatype conversion

--- 2.py
+++ 2.py
@@ -1,2 +1,49 @@
-name = input("enter the name :")
-print("Your name is ",name )
+# variables in python 
+x=int(5.0)
+y="hello"
+print("----->Printing the normal variables")
+print(x)
+print(y)
+
+age = 20
+name = "kedar"
+is_student = str(True)
+print(name)
+print(age)
+print(is_student)
+
+""" data type in python 
+----------> int ,float , str , bool , none   """
+
+# this are the examle of Data Type conversion in python 
+print("----->changeing the type of variable")
+print(type(x)) 
+print(type(name))
+print(type(age))
+print(type(is_student))  # here is the example of the type conversion in python 
+
+# Arithimatic opertion (+ , - , * , / , // , % ,**)
+print("---->Arithmetic opertion ")
+a = 7
+b = 3
+c = a + b
+print(c) # storing the value in a variable in c
+
+ # this one method dirct printing  not storeing the value
+print(a+b) # Addition 
+print(a-b) # Substraction 
+print(a*b) # Multification
+print(a/b) # Devision
+print(a//b) # it will similar to the devision but it will give the hole number 
+print(a%b) # Modules (Reminder)
+print(a**b) # Expentional of the first variable number
+
+
+# Assigning value to multiple variable 
+x, y, z = 10 ,20, 30
+print(x , y , z )
+
+p=q=r=10
+print(p, q, r)
+
+
