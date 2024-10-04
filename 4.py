c356f2d753f76b6fe3f60f786f29cb791839c1b7
Kedar Dhage <kedardhage0@gmail.com>
2024-10-04 17:59:07 +0530

    Opertion in python

--- 4.py
+++ 4.py
@@ -1,1 +1,46 @@
-print("opertion in python")
+# Assigenment opertion :
+print("\nAssigenment opertion:")
+#(=,+=,-=,*=,/=,%=)
+x = 5
+print(x)
+x += 2  # x = x + 1  it will add the 2
+print(x)
+x -= 3 # x = x - 1 it substarte the two
+print(x)
+x *= 9
+print(x)
+x /= 2
+print(x)
+x %= 4  
+print(x)
+
+# comparision opertion 
+# ( == , !=, >, < , >=, <=, )
+
+a , b = 5 ,  10
+print("\ncomparision opertion:")  
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a<=b)
+print(a>=b)
+
+# logical opertion 
+# (And , Or , Not)
+c = 15
+print("\nlogical opertion: ")
+print(a>0 & b > 20)
+print(a>0 or b>20)
+print( not(a>20) )
+
+# membership opertion 
+print("\nmembership opertion:")
+my_list = [12,23,10,45,18,99,63,1]
+Name= "Rockybhai"
+print(2 in my_list)
+print("3" not in Name)
+
+# bitwise opertion 
+# (Bitwise AND, Bitwise OR, Bitwise NOT ,leftshift , rightshift )
+# This opertion widely used in low level programming language & binary opertion 
