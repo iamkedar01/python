d3242bb9f5b23413b98a133099bd49767cb8793e
Kedar Dhage <kedardhage0@gmail.com>
2024-10-04 18:49:47 +0530

    5.py

---
+++ 5.py
@@ -0,0 +1,4 @@
+"""  list in python 8 
+      list: its is collection of different datatype that is stored in in singele variable ,
+            its is ordered,mutable(changeable),duplicate
+        """
