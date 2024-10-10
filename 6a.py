0fe8dcdbee48d19eed1aba3fbc1f190223f708dd
Kedar Dhage <kedardhage0@gmail.com>
2024-10-10 12:08:30 +0530

    6a.py

--- 6a.py
+++ 6a.py
@@ -1,1 +1,4 @@
-# set opertion in python 
+""" set opertion in python 
+    set are the collection of unique items unorderd and unindexed 
+     syntax: my_set = { element1, element2, element3, etc...} """
+
