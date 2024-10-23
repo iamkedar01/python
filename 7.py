8e1c9f397f63c85910ba8df2ca38c44a792717f3
Kedar Dhage <kedardhage0@gmail.com>
2024-10-23 22:09:10 +0530

    7.py

---
+++ 7.py
@@ -0,0 +1,10 @@
+''' Dictionary in the python : A dictionary in Python is a collection of key-value pairs. 
+        you can retrieve or manipulate data using the key. Unlike lists and tuples, dictionaries are unordered and mutable (changeable)
+  1) Creating a Dictionary 
+  2) Accessing Dictionary Elements 
+  3) Adding and Updating Dictionary Elements
+  4) Removing Elements from a Dictionary
+  5) Dictionary Method
+  6) Dictionary Characteristics
+
+'''
