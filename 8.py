b81c3ba35fe784502e4ecb6492aae6905433a22f
Kedar Dhage <kedardhage0@gmail.com>
2024-11-06 21:25:22 +0530

    8.py

---
+++ 8.py
@@ -0,0 +1,10 @@
+''' Conditional Statements in Python: if, elif, and else
+    if-----> The if statement is used to test a condition. If the condition is True, the block of code under the if statement is executed.
+    elif---> The else statement provides an alternative block of code to execute when the if condition is False
+    else---> The elif statement checks another condition if the previous if or elif condition was False
+
+    here uisng the camparision and logical opertions
+    camparision opertions:(==:Equal to, !=:Not equal to,<:Less than,>:Greater than, <=:Less than or equal to,>=:Greater than or equal to )
+    logical opertions:(and:Returns True if both conditions are True,or:Returns True if at least one condition is True,not:Reverses the result of a condition)
+    
+                                                                    '''
