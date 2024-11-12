38a59fae48bcaf06322e3775825e445418f03f4d
Kedar Dhage <kedardhage0@gmail.com>
2024-11-12 19:20:50 +0530

    12.py

---
+++ 12.py
@@ -0,0 +1,11 @@
+# Function 
+''' A function is a reusable block of code that performs a specific task when called.
+ Functions are useful to organize code, make it reusable, and reduce redundancy. '''
+
+# Defining a Function 
+# You define a function using the def keyword followed by the function name, parentheses, and a colon :.
+''' Syntax-->
+           def function_name(parameters):
+                # Block of code   '''
+#Function Parameters 
+# parameters are variables used to pass data into a function 
