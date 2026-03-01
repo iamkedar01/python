c9ff62fca76c9645a4b8126ba099646a64db8411
Kedar Dhage <kedardhage0@gmail.com>
2026-03-01 11:36:29 +0530

    Convert String to Integer and Vice Versa

---
+++ Python-Problems/6ConvertStrintoIntVisaversa.py
@@ -0,0 +1,13 @@
+# String to integer
+num_str = "100" # Takeing the 100 as a string input 
+num_int = int(num_str) # convert the string into intger
+
+print("Converted to integer:", num_int)
+print(type(num_int))
+
+# Integer to string
+num = 200 # Takeing a number 200 
+num_string = str(num) # Converting into a string 
+
+print("Converted to string:", num_string)
+print(type(num_string))
