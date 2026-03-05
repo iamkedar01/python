129256d282c105afdf0fb85f3beb285da7669d13
Kedar Dhage <kedardhage0@gmail.com>
2026-03-05 18:30:37 +0530

    All Arithmetic Operations

---
+++ Python-Problems/10AllArithmeticOperations.py
@@ -0,0 +1,12 @@
+# Take two numbers from user
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Arithmetic operations
+print("Addition:", a + b )        # + adds two numbers
+print("Subtraction:", a - b)     # - subtracts
+print("Multiplication:", a * b)  # * multiplies
+print("Division:", a / b)        # / gives float result
+print("Floor Division:", a // b) # // gives integer division
+print("Modulus:", a % b)         # % gives remainder
+print("Power:", a ** b)          # ** gives a raised to power b
