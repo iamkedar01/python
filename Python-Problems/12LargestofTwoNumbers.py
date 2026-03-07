a132b1cf974e58d86006e37970e00cb49b1d8744
Kedar Dhage <kedardhage0@gmail.com>
2026-03-07 21:13:40 +0530

    Largest of  number

---
+++ Python-Problems/12LargestofTwoNumbers.py
@@ -0,0 +1,9 @@
+# Takeing the two  number from the user 
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Compare using > operator
+if a > b:
+    print("Largest number is:", a)
+else:
+    print("Largest number is:", b)
