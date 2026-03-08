a27e93370ff249db4d32c69ed3074bddbb2cec25
Kedar Dhage <kedardhage0@gmail.com>
2026-03-08 20:49:57 +0530

    Camparison Operator Demo  in Details

---
+++ Python-Problems/13ComparisonOperatorsDemo.py
@@ -0,0 +1,10 @@
+#Takeing the two number from the user
+a = 10
+b = 20
+
+print(a == b)  # Equal to
+print(a != b)  # Not equal to
+print(a > b)   # Greater than
+print(a < b)   # Less than
+print(a >= b)  # Greater than or equal to
+print(a <= b)  # Less than or equal to
