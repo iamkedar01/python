19a40debf6e98d597f90481f800d1499d73789ea
Kedar Dhage <kedardhage0@gmail.com>
2026-03-09 20:38:28 +0530

    Logical Oprator in Python

---
+++ Python-Problems/14LogicalOperators.py
@@ -0,0 +1,11 @@
+a = 10
+b = 20
+
+# AND → True if both conditions are True
+print(a > 5 and b > 15)
+
+# OR → True if at least one condition is True
+print(a > 5 or b < 10)
+
+# NOT → Reverses the result
+print(not(a > 5))
