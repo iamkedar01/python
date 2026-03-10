d142f5212418321260709342b3adf7e9b1348b4d
Kedar Dhage <kedardhage0@gmail.com>
2026-03-10 20:46:16 +0530

    Reminder finding by number 5

---
+++ Python-Problems/15RemainderWhenDividedby5.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number: "))
+
+# Take teh number from the user and devide by the 5 and give the reminder
+remainder = num % 5  # % gives remainder
+
+
+print("Remainder when divided by 5:", remainder)
