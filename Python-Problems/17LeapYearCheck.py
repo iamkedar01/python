54765db6c6e85441ea697ebe4744a6acdc280fb3
Kedar Dhage <kedardhage0@gmail.com>
2026-03-12 18:39:58 +0530

    Checking a leap Year or not

---
+++ Python-Problems/17LeapYearCheck.py
@@ -0,0 +1,7 @@
+year = int(input("Enter year: "))
+
+# Apply leap year condition
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("Leap Year")
+else:
+    print("Not a Leap Year")
