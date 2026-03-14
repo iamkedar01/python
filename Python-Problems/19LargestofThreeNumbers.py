a74fb7059a8aa00b3d0a7533e72fcd64aa8810c7
Kedar Dhage <kedardhage0@gmail.com>
2026-03-14 14:20:38 +0530

    Largest of three number

---
+++ Python-Problems/19LargestofThreeNumbers.py
@@ -0,0 +1,12 @@
+# Take three numbers from user
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+# Compare using logical AND
+if a > b and a > c:
+    print("Largest number is:", a)
+elif b > a and b > c:
+    print("Largest number is:", b)
+else:
+    print("Largest number is:", c)
