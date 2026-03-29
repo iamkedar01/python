276093894c8cb8d725eeed8f3a21ffb206a413ba
Kedar Dhage <kedardhage0@gmail.com>
2026-03-29 12:01:50 +0530

    Reverse the given Number

---
+++ Python-Problems/32ReverseaNumber.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number: "))
+
+reverse = 0
+
+while num > 0:
+    digit = num % 10          # Get last digit
+    reverse = reverse * 10 + digit
+    num = num // 10           # Remove last digit
+
+print("Reversed number:", reverse)
