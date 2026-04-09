4eeb915944f330b2ebccd43b5bf198f743f5c90e
Kedar Dhage <kedardhage0@gmail.com>
2026-04-09 21:19:52 +0530

    Replace the words in Sentence

---
+++ Python-Problems/41ReplaceaWord.py
@@ -0,0 +1,7 @@
+# Take the input form the user 
+text = input("Enter sentence: ")
+
+# replace the exist  words in sentence by the new word
+new_text = text.replace("bad", "good")
+
+print("Updated sentence:", new_text) 
