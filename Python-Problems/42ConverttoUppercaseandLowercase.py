dd8e44e703ff705eae3de0ff70aae37713625f99
Kedar Dhage <kedardhage0@gmail.com>
2026-04-11 22:14:34 +0530

    Convert  the uppercase and Lowercase

---
+++ Python-Problems/42ConverttoUppercaseandLowercase.py
@@ -0,0 +1,5 @@
+
+text = input("Enter text: ")
+# take the input from the user and convert the given text into uppercase and lowercase using the methods
+print("Uppercase:", text.upper())
+print("Lowercase:", text.lower())
