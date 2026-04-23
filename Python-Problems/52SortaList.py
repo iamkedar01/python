893e6d324bad9ee36355ac9332db3ecb3959e1d2
Kedar Dhage <kedardhage0@gmail.com>
2026-04-23 21:44:37 +0530

    Sorting the given list both Asec and Desc order

--- Python-Problems/52SortaList.py
+++ Python-Problems/52SortaList.py
@@ -2,5 +2,9 @@ numbers = [5, 3, 8, 1, 2]
 
 # Sort ascending
 numbers.sort()
-
 print("Sorted list:", numbers)
+#The above code will aragnge in the normal sorting in order
+
+# the below code was for the arrangeing the in reverse order
+numbers.sort(reverse=True)
+print("Descending order:", numbers)
